051c59b9fb97d22e47af46c9661d5a0556e1e916
박기완 <zprime0920@gmail.com>
2018-09-01 18:55:26 +0900

    Refactor: convert ./app/configureStore.js to .ts

--- app/configureStore.ts
+++ app/configureStore.ts
@@ -2,15 +2,28 @@
  * Create the store with dynamic reducers
  */
 
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, Store } from 'redux';
 import { fromJS } from 'immutable';
 import { routerMiddleware } from 'react-router-redux';
 import createSagaMiddleware from 'redux-saga';
 import createReducer from './reducers';
+import { History } from 'history';
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: (option: { shouldHotReload: boolean }) => typeof compose;
+  }
+}
+
+interface IExtendedStre extends Store {
+  runSaga: typeof sagaMiddleware.run;
+  injectedReducers: any; // FIXME
+  injectedSagas: any; // FIXME
+}
 
 const sagaMiddleware = createSagaMiddleware();
 
-export default function configureStore(initialState = {}, history) {
+export default function configureStore(initialState = {}, history: History) {
   // Create the store with two middlewares
   // 1. sagaMiddleware: Makes redux-sagas work
   // 2. routerMiddleware: Syncs the location/URL path to the state
@@ -19,7 +32,7 @@ export default function configureStore(initialState = {}, history) {
   const enhancers = [applyMiddleware(...middlewares)];
 
   // If Redux DevTools Extension is installed use it, otherwise use Redux compose
-  /* eslint-disable no-underscore-dangle, indent */
+  /* tslint-disable no-underscore-dangle, indent */
   const composeEnhancers =
     process.env.NODE_ENV !== 'production' &&
     typeof window === 'object' &&
@@ -30,9 +43,9 @@ export default function configureStore(initialState = {}, history) {
           shouldHotReload: false,
         })
       : compose;
-  /* eslint-enable */
+  /* tslint-enable */
 
-  const store = createStore(
+  const store: IExtendedStre = createStore(
     createReducer(),
     fromJS(initialState),
     composeEnhancers(...enhancers),
--- app/tests/store.test.js
+++ app/tests/store.test.js
@@ -3,7 +3,7 @@
  */
 
 import { browserHistory } from 'react-router-dom';
-import configureStore from '../configureStore';
+import configureStore from '../configureStore.ts';
 
 describe('configureStore', () => {
   let store;
